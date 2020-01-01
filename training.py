e063f06f0f18e68e6751ddc46dcdd3551c42d9fa
wyx <wyx_ei@sjtu.edu.cn>
2020-01-01 10:32:13 +0800

    update gridsearch

--- training.py
+++ training.py
@@ -1,17 +1,38 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import ensemble
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
 from sklearn.metrics import mean_squared_error 
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
 import joblib
+import time
+from collections import Counter
 
-data = pd.read_csv("training_data1.csv")
-x, y = data.iloc[:, :12], data.iloc[:, 13]
-# print(data.iloc[:, 13])
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.01, random_state=0)
-clf = DecisionTreeRegressor(splitter="random", max_depth=24, min_samples_split=12, min_samples_leaf=6)
+from sklearn.model_selection import train_test_split 
+data = pd.read_csv("training_data.csv")
+x, y = data.iloc[:470000, :14], data.iloc[:470000, 14]
+
+rus = RandomUnderSampler(sampling_strategy={1: 3*Counter(y)[-1]})
+x1, y1 = rus.fit_sample(x, y)
+# print(Counter(y1))
+
+x2, y2 = x1.iloc[:, :13], x1.iloc[:, 13]
+y2 = y2*100
+
+x_train, x_test, y_train, y_test = train_test_split(x2, y2, test_size=0.2, random_state=1)
+
+forest_para = {'max_depth':list(range(15, 26)), 'oob_score':[True, False], 'min_samples_leaf':list(range(3,10))}
+
+clf = GridSearchCV(RandomForestRegressor(), param_grid=forest_para)
 clf.fit(x_train, y_train)
-joblib.dump(clf, 'decision_tree.model')
+
+clf = clf.best_estimator_
+joblib.dump(clf, 'random_forest.model')
 
 mse = mean_squared_error(y_test, clf.predict(x_test))
 print("MSE: %4f" % mse)
+# print(time.time()-t)
