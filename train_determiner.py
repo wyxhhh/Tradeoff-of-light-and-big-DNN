78a3fdbb2bea10379019a84fc4f7957246af3756
Rogerity <58288642+Rogerity@users.noreply.github.com>
2020-01-10 14:39:14 +0800

    Update and rename training.py to train_determiner.py

--- train_determiner.py
+++ train_determiner.py
@@ -14,17 +14,19 @@ from collections import Counter
 
 from sklearn.model_selection import train_test_split 
 data = pd.read_csv("training_data.csv")
-x, y = data.iloc[:470000, :14], data.iloc[:470000, 14]
+x2, y2 = data.iloc[:470000, :13], data.iloc[:470000, 13]
 
-rus = RandomUnderSampler(sampling_strategy={1: 3*Counter(y)[-1]})
-x1, y1 = rus.fit_sample(x, y)
-# print(Counter(y1))
+# down-sampling step, which is not so helpful
+# rus = RandomUnderSampler(sampling_strategy={1: 3*Counter(y)[-1]})
+# x1, y1 = rus.fit_sample(x, y)
+# # print(Counter(y1))
 
-x2, y2 = x1.iloc[:, :13], x1.iloc[:, 13]
+# x2, y2 = x1.iloc[:, :13], x1.iloc[:, 13]
 y2 = y2*100
 
 x_train, x_test, y_train, y_test = train_test_split(x2, y2, test_size=0.2, random_state=1)
 
+# an example of training random_forest 
 forest_para = {'max_depth':list(range(15, 26)), 'oob_score':[True, False], 'min_samples_leaf':list(range(3,10))}
 
 clf = GridSearchCV(RandomForestRegressor(), param_grid=forest_para)
